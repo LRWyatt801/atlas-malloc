43c824e98e5a00e91fa40f716cbde0acfb9f6ed3
LRWyatt801 <loganwyatt1995@gmail.com>
2025-01-20 16:42:22 -0600

    Add malloc.h and naive_malloc.c

---
+++ malloc.h
@@ -0,0 +1,12 @@
+#ifndef MALLOC_H
+#define MALLOC_H
+
+/***** LIBRARIES *****/
+
+#include <cstddef>
+
+/***** PROTOTYPES *****/
+
+void *naive_malloc(size_t size);
+
+#endif /* MALLOC_H */
---
+++ naive_malloc.c
@@ -0,0 +1,4 @@
+#include "malloc.h"
+
+/**
+* naive_malloc - 
