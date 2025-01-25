29c59133cd43f12b0642e1034206860fae980d5d
LRWyatt801 <loganwyatt1995@gmail.com>
2025-01-25 15:11:23 -0600

    Update for new_chunk init and documentation

--- naive_malloc.c
+++ naive_malloc.c
@@ -39,11 +39,15 @@ void *naive_malloc(size_t size)
 	void *new_chunk;
 	chunk_hdr_t *new_chunk_hdr;
 
+	/* Set new_chunk == to end of allocated mem */
+	new_chunk = nxt_chunk;
+
 	new_chunk_hdr = (chunk_hdr_t *)new_chunk;
 	size = ALIGNSIZE(size);
 
 	new_chunk_hdr->size = size;
 
+	/* update nxt_chunk to end of allocated mem */
 	nxt_chunk = (char *)new_chunk + NHDR_SIZE + size;
 
 	return ((void *)((char *)new_chunk + NHDR_SIZE));
